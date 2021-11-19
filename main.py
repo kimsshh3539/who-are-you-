b9489163688c7c80bfddb0aaac05b1c03cc6bbb7
Kimzonzal1785 <84667818+kimsshh3539@users.noreply.github.com>
2021-11-19 13:59:34 +0900

    Update main.py

--- main.py
+++ main.py
@@ -20,9 +20,9 @@ async def on_message(message):
         return None
     else:
         if message.content.startswith('!전적'):
-            Name = message.content.split(" ")
-            #space = Name[1]
-            url = 'https://www.op.gg/summoner/userName=' + Name
+            Name = message.content[3:len(message.content)]
+            space = Name[1]
+            url = 'https://www.op.gg/summoner/userName=' + space
             SummonerName = ''
             Tier = []
             TierUnranked = 'false'
